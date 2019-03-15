a0ab64d1917bc4c73a3e60d68cb44abe57e918b3
Mateus Pereira <mateus.silva@datainfo.inf.br>
2019-03-15 14:40:29 -0300

    implementação de controller e view de cadastro e listagem

--- src/main/java/com/eventos/controller/homeController.java
+++ src/main/java/com/eventos/controller/homeController.java
@@ -1,17 +1,40 @@
 package com.eventos.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventos.models.Evento;
+import com.eventos.repositoty.EventoRepository;
 
 
 @Controller
 
 public class homeController {
+	@Autowired
+	private EventoRepository er;
 	
-	@RequestMapping("/cadastrarEvento")
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form() {
+		
 		return "evento/formEvento";
 	}
 	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Evento evento) {
+		er.save(evento);
+		return "redirect:/listaEventos";
+		
+	}
+	
+	@RequestMapping(value="/listaEventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("evento/listaEventos");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
 }
